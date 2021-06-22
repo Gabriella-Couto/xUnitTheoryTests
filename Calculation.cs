cf125b9ee84e6dc69158f716eea7af3e367f5ce0
Gabriella Souza do Couto <gabriella@atlasgov.com>
2021-06-22 14:40:19 -0300

    Uploading the project to the repo

---
+++ Calculation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculations
+{
+    public class Calculation
+    {
+        public bool isOddNumber (int n)
+        {
+            return (n % 2) == 1;
+        }
+
+        public int sumTwoValues(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+    }
+}
